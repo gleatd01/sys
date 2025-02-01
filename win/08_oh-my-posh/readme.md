a82a111d5f17c46f0e176044b7349a1d837aec55
Taylor Gleason <taylor@buzzedtop.com>
2025-01-31 21:28:06 -0500

    updates for installing major tools in order

---
+++ win/08_oh-my-posh/readme.md
@@ -0,0 +1,1 @@
+oh-my-posh init pwsh --config "https://raw.githubusercontent.com/gleatd01/sys/refs/heads/main/win/oh-my-posh/mod-kali.omp.json" | Invoke-Expression
---
+++ win/08_oh-my-posh/setup.ps1
@@ -0,0 +1,2 @@
+winget install --id JanDeDobbeleer.OhMyPosh --source winget
+oh-my-posh init pwsh --config "https://raw.githubusercontent.com/gleatd01/sys/refs/heads/main/win/oh-my-posh/mod-kali.omp.json" | Invoke-Expression
